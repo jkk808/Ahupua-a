ce0bf381eb4ee357a08c066c111b751fef7401f4
jkapali <jaden.kapali@gmail.com>
2025-03-04 13:59:06 -1000

    init home and about tutorial

---
+++ dashboard/web/src/pages/AboutPage/AboutPage.jsx
@@ -0,0 +1,34 @@
+import { Link, routes } from '@redwoodjs/router'
+import { Metadata } from '@redwoodjs/web'
+
+const AboutPage = () => {
+  return (
+    <>
+      <Metadata title="About" description="About page" />
+
+      <h1>AboutPage</h1>
+      <p>
+        Find me in <code>./web/src/pages/AboutPage/AboutPage.jsx</code>
+      </p>
+      <header>
+        <h1>Redwood Blog</h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to={routes.about()}>About</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main>
+        <p>
+          This site was created to demonstrate my mastery of Redwood: Look on my
+          works, ye mighty, and despair!
+        </p>
+        <Link to={routes.home()}>Return home</Link>
+      </main>
+    </>
+  )
+}
+
+export default AboutPage
---
+++ dashboard/web/src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,29 @@
+import { Link, routes } from '@redwoodjs/router'
+import { Metadata } from '@redwoodjs/web'
+
+const HomePage = () => {
+  return (
+    <>
+      <Metadata title="Home" description="Home page" />
+
+      <h1>HomePage</h1>
+      <p>
+        Find me in <code>./web/src/pages/HomePage/HomePage.jsx</code>
+      </p>
+      
+      <header>
+        <h1>Redwood Blog</h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to={routes.about()}>About</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main>Home</main>
+    </>
+  )
+}
+
+export default HomePage
