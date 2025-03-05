72c29e598e691d65c9baaa501c4bb143f3169e78
jkapali <jaden.kapali@gmail.com>
2025-03-04 15:59:18 -1000

    header and side panel implemented

--- dashboard/web/src/layouts/MainPanelsLayout/MainPanelsLayout.jsx
+++ dashboard/web/src/layouts/MainPanelsLayout/MainPanelsLayout.jsx
@@ -1,19 +1,19 @@
 import { Link, routes } from '@redwoodjs/router'
+import SidePanel from 'src/components/Panels/SidePanel/SidePanel'
+import TopPanel from 'src/components/Panels/TopPanel/TopPanel'
+import React from 'react';
 
 const MainPanelLayout = ({ children }) => {
   return (
     <>
-      <header>
-        <h1>Tesst</h1>
-        <nav>
-          <ul>
-            <li>
-              <Link to={routes.about()}>About</Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
-      <main>{children}</main>
+      <div className="flex flex-col w-full h-screen bg-gray-50">
+      <TopPanel />        
+
+        <main>          
+          <SidePanel />
+        </main>
+
+      </div>
     </>
   )
 }
