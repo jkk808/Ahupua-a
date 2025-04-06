0c72796ddcdc5917c586f7c6f703a2916567260a
Hysk14 <straightliving14@gmail.com>
2025-04-05 14:55:16 -1000

    routed "map" button in SidePanel.jsx to MapPage.jsx

--- web/src/components/Panels/SidePanel/SidePanel.jsx
+++ web/src/components/Panels/SidePanel/SidePanel.jsx
@@ -9,52 +9,52 @@ const SidePanel = () => {
   return (
     <div className="fixed left-0 h-full w-64 bg-white border-r border-gray-200 p-6">
 
-        {/* holds the links to different pages for water and soil */}
-        {/* <HealthScoresCell></HealthScoresCell> */}
+      {/* holds the links to different pages for water and soil */}
+      {/* <HealthScoresCell></HealthScoresCell> */}
 
-        <div className="space-y-4 pt-4">
-          <button
+      <div className="space-y-4 pt-4">
+        <button
           onClick={toggleDropdown}
           className="border border-gray-200 drop-shadow-sm w-full text-center px-4 py-3 hover:bg-gray-50 rounded-lg transition-colors">
-              Patches
-          </button>
+          Patches
+        </button>
 
-          {open && (
-            <div className="origin-top-right mt-2 w-full">
-              <Patch></Patch>
-              <Patch></Patch>
-              <Patch></Patch>
-              <Patch></Patch>
-              <Patch></Patch>
-              <Patch></Patch>
-              <Patch></Patch>
-              <Patch></Patch>
-              <Patch></Patch>
-              <Patch></Patch>
-              <Patch></Patch>
-              <Patch></Patch>
-            </div>
-          )}
-          <div className=''>
-            <button className="border border-gray-200 drop-shadow-sm w-full text-center px-4 py-3 hover:bg-gray-50 rounded-lg transition-colors">
-              Recommendations
-            </button>
+        {open && (
+          <div className="origin-top-right mt-2 w-full">
+            <Patch></Patch>
+            <Patch></Patch>
+            <Patch></Patch>
+            <Patch></Patch>
+            <Patch></Patch>
+            <Patch></Patch>
+            <Patch></Patch>
+            <Patch></Patch>
+            <Patch></Patch>
+            <Patch></Patch>
+            <Patch></Patch>
+            <Patch></Patch>
           </div>
+        )}
+        <div className=''>
           <button className="border border-gray-200 drop-shadow-sm w-full text-center px-4 py-3 hover:bg-gray-50 rounded-lg transition-colors">
-            All Sensors
+            Recommendations
           </button>
-          <button className="border border-gray-200 drop-shadow-sm w-full text-center px-4 py-3 hover:bg-gray-50 rounded-lg transition-colors">
-            <Link to={routes.map()}>
-            Map
-            </Link>
-          </button>
-          {/* <button className="border border-gray-200 drop-shadow-sm w-full text-center px-4 py-3 hover:bg-gray-50 rounded-lg transition-colors">
+        </div>
+        <button className="border border-gray-200 drop-shadow-sm w-full text-center px-4 py-3 hover:bg-gray-50 rounded-lg transition-colors">
+          All Sensors
+        </button>
+        <Link to={routes.map()}
+          className="block border border-gray-200 drop-shadow-sm w-full text-center px-4 py-3 mt-6 hover:bg-gray-50 rounded-lg transition-colors"
+        >
+          Map
+        </Link>
+        {/* <button className="border border-gray-200 drop-shadow-sm w-full text-center px-4 py-3 hover:bg-gray-50 rounded-lg transition-colors">
             Export Data
           </button> */}
-          {/* <button className="border border-gray-200 drop-shadow-sm w-full text-center px-4 py-3 hover:bg-gray-50 rounded-lg transition-colors">
+        {/* <button className="border border-gray-200 drop-shadow-sm w-full text-center px-4 py-3 hover:bg-gray-50 rounded-lg transition-colors">
             Help
           </button> */}
-        </div>
+      </div>
 
     </div>
   )
