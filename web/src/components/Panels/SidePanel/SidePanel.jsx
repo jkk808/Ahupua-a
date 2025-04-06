e6e3606c7136605ef918cfef39430919d473f3de
Jeff Wong <42687471+jeff-yc-wong@users.noreply.github.com>
2025-04-05 23:05:40 -1000

    fixed buttons only working when clicking on the text

--- web/src/components/Panels/SidePanel/SidePanel.jsx
+++ web/src/components/Panels/SidePanel/SidePanel.jsx
@@ -16,70 +16,68 @@ const SidePanel = () => {
   const [open, setOpen] = useState(false)
   const toggleDropdown = () => setOpen((prev) => !prev)
   return (
-    <div className="fixed left-0 h-full w-64 bg-white border-r border-gray-200 p-6">
-
+    <div className="fixed left-0 h-full w-64 border-r border-gray-200 bg-white p-6">
       {/* holds the links to different pages for water and soil */}
       {/* <HealthScoresCell></HealthScoresCell> */}
 
-        <div className="space-y-4">
+      <div className="space-y-4">
+        <div className="w-full rounded-lg border border-gray-200 p-2 text-left drop-shadow-sm transition-colors hover:bg-gray-50">
+          <h3 className="mb-2 text-sm font-medium text-gray-500">
+            Stream Health
+          </h3>
+          <HealthScore score={0.9}></HealthScore>
+        </div>
 
-            <div className='border border-gray-200 drop-shadow-sm w-full text-left p-2 hover:bg-gray-50 rounded-lg transition-colors'>
-              <h3 className="text-sm font-medium text-gray-500 mb-2">
-              Stream Health
-              </h3>
-              <HealthScore score={0.9}></HealthScore>
-            </div>
+        <div className="w-full rounded-lg border border-gray-200 p-2 text-left drop-shadow-sm transition-colors hover:bg-gray-50">
+          <h3 className="mb-2 text-sm font-medium text-gray-500">
+            Overall Soil Health
+          </h3>
+          <HealthScore score={0.6}></HealthScore>
+        </div>
 
-            <div className='border border-gray-200 drop-shadow-sm w-full text-left p-2 hover:bg-gray-50 rounded-lg transition-colors'>
-              <h3 className="text-sm font-medium text-gray-500 mb-2">
-              Overall Soil Health
-              </h3>
-              <HealthScore score={0.6}></HealthScore>
-            </div>
+        <br></br>
 
-          <br></br>
-
-          <Link to={routes.home()}>
-            <button
+        <Link to={routes.home()}>
+          <button
             onClick={toggleDropdown}
-            className="border border-gray-200 drop-shadow-sm w-full text-center px-4 py-3 hover:bg-gray-50 rounded-lg transition-colors">
-                Patches
-            </button>
-          </Link>
+            className="w-full rounded-lg border border-gray-200 px-4 py-3 text-center drop-shadow-sm transition-colors hover:bg-gray-50"
+          >
+            Patches
+          </button>
+        </Link>
 
-          {open && (
-            <div className="origin-top-right mt-2 w-full">
-              {patches.map((patch) => (
-                <Link
-                  to={routes.patch({ location: patch.location })}
-                  className="block px-4 py-2 border border-gray-200 rounded hover:bg-gray-100 text-sm"
-                >
-                  {patch.location}
-                </Link>
-              ))}
-            </div>
-          )}
+        {open && (
+          <div className="mt-2 w-full origin-top-right">
+            {patches.map((patch) => (
+              <Link
+                to={routes.patch({ location: patch.location })}
+                className="block rounded border border-gray-200 px-4 py-2 text-sm hover:bg-gray-100"
+              >
+                {patch.location}
+              </Link>
+            ))}
+          </div>
+        )}
 
-          <button className="border border-gray-200 drop-shadow-sm w-full text-center px-4 py-3 hover:bg-gray-50 rounded-lg transition-colors">
-            <Link to={routes.huliʻia()}>
+        <Link to={routes.huliʻia()}>
+          <button className="mt-4 w-full rounded-lg border border-gray-200 px-4 py-3 text-center drop-shadow-sm transition-colors hover:bg-gray-50">
             Kilo
-            </Link>
           </button>
+        </Link>
 
-          <button className="border border-gray-200 drop-shadow-sm w-full text-center px-4 py-3 hover:bg-gray-50 rounded-lg transition-colors">
-            <Link to={routes.map()}>
+        <Link to={routes.map()}>
+          <button className="mt-4 w-full rounded-lg border border-gray-200 px-4 py-3 text-center drop-shadow-sm transition-colors hover:bg-gray-50">
             Map
-            </Link>
           </button>
+        </Link>
 
-          {/* <button className="border border-gray-200 drop-shadow-sm w-full text-center px-4 py-3 hover:bg-gray-50 rounded-lg transition-colors">
+        {/* <button className="border border-gray-200 drop-shadow-sm w-full text-center px-4 py-3 hover:bg-gray-50 rounded-lg transition-colors">
             Export Data
           </button> */}
         {/* <button className="border border-gray-200 drop-shadow-sm w-full text-center px-4 py-3 hover:bg-gray-50 rounded-lg transition-colors">
             Help
           </button> */}
-        </div>
-
+      </div>
     </div>
   )
 }
