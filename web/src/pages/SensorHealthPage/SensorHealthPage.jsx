c1586d3ab1b178fd5f17997dd323099e3700aaef
jkapali <jaden.kapali@gmail.com>
2025-04-05 23:17:28 -1000

    charts of current and voltage from a sensor

--- web/src/pages/SensorHealthPage/SensorHealthPage.jsx
+++ web/src/pages/SensorHealthPage/SensorHealthPage.jsx
@@ -1,16 +1,16 @@
 // import { Link, routes } from '@redwoodjs/router'
 import { Metadata } from '@redwoodjs/web'
+import GraphCell from 'src/components/Graph/GraphCell'
 
 const SensorHealthPage = () => {
   return (
     <>
       <Metadata title="SensorHealth" description="SensorHealth page" />
 
-      <h1>SensorHealthPage</h1>
-      <p>
-        Find me in{' '}
-        <code>./web/src/pages/SensorHealthPage/SensorHealthPage.jsx</code>
-      </p>
+      <div className='space-y-16'>
+        <GraphCell type={'cur'}></GraphCell>
+        <GraphCell type={'volt'}></GraphCell>
+      </div>
       {/*
            My default route is named `sensorHealth`, link to me with:
            `<Link to={routes.sensorHealth()}>SensorHealth</Link>`
