f5db5d54cfd46b985ebee6fe7c36f1449c350108
Jeff Wong <42687471+jeff-yc-wong@users.noreply.github.com>
2025-04-06 00:22:10 -1000

    made each patch summary go to the patch page on click

--- web/src/components/Patch/Patch.jsx
+++ web/src/components/Patch/Patch.jsx
@@ -1,8 +1,9 @@
+import { Link, routes } from "@redwoodjs/router"
 import HealthScore from "../HealthScore/HealthScore"
 
 const Patch = ({ patch, scores }) => {
   return (
-    <>
+    <Link to={routes.patch({ location: patch.name })}>
       <button className='border border-gray-200 drop-shadow-sm w-full h-64 text-left p-2 hover:bg-gray-50 rounded-lg transition-colors'>
         <h1 className="text-xl font-medium text-gray-500 mb-2 ml-6">
           {patch.name}
@@ -23,7 +24,7 @@ const Patch = ({ patch, scores }) => {
           </div>
         </div>
       </button>
-    </>
+    </Link>
   )
 }
 
